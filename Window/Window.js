939386f7d673e05299a31702f338dc31cd44d495
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2013-02-25 00:03:30 -0500

    Adding initial windowing streams examples

--- Window/Window.js
+++ Window/Window.js
@@ -1,6 +1,17 @@
-var JSONStream = require('JSONStream'),
-	WindowStream = require('window-stream');
+var fs = require('fs'),
+	JSONStream = require('JSONStream'),
+	EventWindow = require('window-stream').EventWindow;
 
+var ev = new EventWindow({ size: 10});
+
+// Calculate mean
+ev.on('data', function (data) {
+	console.log(data.length);
+});
+
+fs.createReadStream('../npm.json')
+    .pipe(JSONStream.parse(['rows', true, 'doc', 'description']))
+    .pipe(ev);
 
 
 
